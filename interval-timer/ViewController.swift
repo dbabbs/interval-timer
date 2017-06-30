01d0a934e8707230b1ba9f0191cda2ec091ee0d1
dbabbs <dbabbs@uw.edu>
2017-06-30 13:08:02 -0700

    add circular progress ring pod

--- interval-timer/ViewController.swift
+++ interval-timer/ViewController.swift
@@ -14,13 +14,18 @@ class ViewController: UIViewController {
 
     //UI
     var gradient = CAGradientLayer()
+    
     @IBOutlet weak var profileImage: UIImageView!
-    
     @IBOutlet weak var triggerButton: UIButton!
-
     @IBOutlet weak var outerCircleOne: UIButton!
-    
     @IBOutlet weak var outerCircleTwo: UIButton!
+    @IBOutlet weak var currentItem: UILabel!
+    @IBOutlet weak var currentSubTimeLabel: UILabel!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    @IBOutlet weak var intervalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,17 +36,22 @@ class ViewController: UIViewController {
         triggerButton.layer.cornerRadius = triggerButton.frame.width / 2
         triggerButton.layer.masksToBounds = true
         
+        
+        //outlines
         outerCircleOne.layer.cornerRadius = outerCircleOne.frame.width / 2
         outerCircleTwo.layer.cornerRadius = outerCircleTwo.frame.width / 2
-        
-
-        
         outerCircleOne.layer.borderWidth = 10
         outerCircleTwo.layer.borderWidth = 10
-
         outerCircleOne.layer.borderColor = colors.lightGrey.cgColor
         outerCircleTwo.layer.borderColor = colors.lightGrey.cgColor
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if track {
+            black()
+        } else {
+            white()
+        }
     }
     
     
@@ -60,12 +70,26 @@ class ViewController: UIViewController {
     }
     
     func black() {
-        //self.view.backgroundColor = colors.black
+        //Unhide labels & change color
+        currentItem.isHidden = false
+        totalTimeLabel.isHidden = false
+        currentItem.textColor = colors.white
+        totalTimeLabel.textColor = colors.white
+        intervalLabel.textColor = colors.white
+        currentSubTimeLabel.isHidden = false
+        currentSubTimeLabel.textColor = colors.white
+        
+        //Main button
+        triggerButton.backgroundColor = UIColor(white: 1, alpha: 0.0)
+        triggerButton.setTitleColor(UIColor(white: 1, alpha: 0.0), for: UIControlState.normal)
+        
+        //outer circles
+        outerCircleOne.layer.borderColor = colors.blue.cgColor
+        outerCircleTwo.layer.borderColor = colors.red.cgColor
         
         let colorTop =  colors.blackStart.cgColor
         let colorBottom = colors.black.cgColor
-        
-        gradient.colors = [ colorTop, colorBottom]
+        gradient.colors = [colorTop, colorBottom]
         gradient.locations = [ 0.0, 1.0]
         gradient.frame = self.view.bounds
         
@@ -75,6 +99,21 @@ class ViewController: UIViewController {
     }
     
     func white() {
+        //Hide labels & change color
+        currentItem.isHidden = true
+        totalTimeLabel.isHidden = true
+        intervalLabel.textColor = colors.black
+        currentSubTimeLabel.isHidden = true
+        
+        //Main button
+        triggerButton.backgroundColor = colors.blue
+        triggerButton.setTitleColor(colors.white, for: UIControlState.normal)
+        
+        //outer circles
+        outerCircleOne.layer.borderColor = colors.lightGrey.cgColor
+        outerCircleTwo.layer.borderColor = colors.lightGrey.cgColor
+        
+        //Set background
         gradient.removeFromSuperlayer()
         self.view.backgroundColor = colors.white
         transition(item: self.view)
