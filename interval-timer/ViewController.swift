d22add6147b94ffa92b17c2222b2ecc0e376686b
dbabbs <dbabbs@uw.edu>
2017-06-29 19:47:39 -0700

    begin interval-timer redo

--- interval-timer/ViewController.swift
+++ interval-timer/ViewController.swift
@@ -11,77 +11,37 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    let workoutSegueIdentifier = "showWorkoutSegue"
+
+    //UI
+    var gradient = CAGradientLayer()
+    @IBOutlet weak var profileImage: UIImageView!
     
-    var timer2 = Timer()
-    var startTime2 = TimeInterval()
+    @IBOutlet weak var triggerButton: UIButton!
+
+    @IBOutlet weak var outerCircleOne: UIButton!
     
-    var timer = Timer()
-    var startTime = TimeInterval()
-    var player: AVAudioPlayer?
-    
-    var activity:Array< String > = Array < String >()
-    var time:Array< String > = Array < String >()
-    
-    @IBOutlet weak var currentActivityLabel: UILabel!
-    @IBOutlet weak var nextActivityLabel: UILabel!
-    
-    
-    @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var intervalText: UILabel!
-    @IBOutlet weak var progressView: UIProgressView!
-    @IBOutlet weak var musicStatus: UILabel!
-    
-    @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var stopButton: UIButton!
-    
-    var voiceOptions = ["🔇", "🔊", "👄", "🇬🇧"]
-    var voiceStatus = "👄"
-    var z = 2
-    
-    
-    var interval = 30
-    var music = true;
-    var paused = true;
-    var i = 4
-    let intervalOptions = [5, 10, 15, 20, 30, 60, 90, 120]
-    var instance = 1
-    var pausedTime: TimeInterval = NSDate.timeIntervalSinceReferenceDate
-    
+    @IBOutlet weak var outerCircleTwo: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //UI
+        profileImage.layer.cornerRadius = profileImage.frame.width / 2
+        profileImage.layer.masksToBounds = true
         
+        triggerButton.layer.cornerRadius = triggerButton.frame.width / 2
+        triggerButton.layer.masksToBounds = true
         
-        get_data_from_url("https://dbabbs.github.io/interval-timer/workout.json")
-        print("The music value is set to: \(music)")
+        outerCircleOne.layer.cornerRadius = outerCircleOne.frame.width / 2
+        outerCircleTwo.layer.cornerRadius = outerCircleTwo.frame.width / 2
         
+
         
-        progressView.setProgress(0, animated: false)
-        intervalText.text = "\(interval)"
-        musicStatus.text = voiceStatus
-        
-        startButton.backgroundColor = UIColor(red: 0/255, green: 118/255, blue: 255/255, alpha: 1)
-        startButton.layer.cornerRadius = 0.5 * startButton.bounds.size.width
-        //stopButton.backgroundColor = UIColor(red: 61/255, green: 61/255, blue: 61/255, alpha: 1)
-        stopButton.layer.cornerRadius = 0.5 * stopButton.bounds.size.width
-        
-        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.intervalSelect))
-        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.intervalSelect))
-        leftSwipe.direction = UISwipeGestureRecognizerDirection.left
-        rightSwipe.direction = UISwipeGestureRecognizerDirection.right
-        view.addGestureRecognizer(leftSwipe)
-        view.addGestureRecognizer(rightSwipe)
-        
-        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.musicControl))
-        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.musicControl))
-        downSwipe.direction = UISwipeGestureRecognizerDirection.down
-        upSwipe.direction = UISwipeGestureRecognizerDirection.up
-        view.addGestureRecognizer(downSwipe)
-        view.addGestureRecognizer(upSwipe)
-        
-        
-        
+        outerCircleOne.layer.borderWidth = 10
+        outerCircleTwo.layer.borderWidth = 10
+
+        outerCircleOne.layer.borderColor = colors.lightGrey.cgColor
+        outerCircleTwo.layer.borderColor = colors.lightGrey.cgColor
+
     }
     
     
@@ -90,259 +50,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func start(_ sender: Any) {
-        self.currentActivityLabel.text = activity[0]
-        self.nextActivityLabel.text = activity[1]
-        if paused == false {
-            startButton.setTitle("Start",for: .normal)
-            paused = true;
-            timer.invalidate()
-            timer2.invalidate()
-            
+    @IBAction func trigger(_ sender: UIButton) {
+        if !track {
+            black()
         } else {
-            startButton.setTitle("Pause",for: .normal)
-            timer = Timer.scheduledTimer(timeInterval: 0.01, target:self, selector: #selector(ViewController.updateCounterText), userInfo: nil, repeats: true)
-            startTime = NSDate.timeIntervalSinceReferenceDate - parseDuration(timerLabel.text!)
-            timer2 = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
-            startTime2 = NSDate.timeIntervalSinceReferenceDate
-            paused = false;
+            white()
         }
+        track = !track
     }
     
-    func speak(word : String) {
-        let utterance = AVSpeechUtterance(string: word)
-        var language = ""
-        if voiceStatus == "🇬🇧" {
-            language = "en-gb"
-        } else if voiceStatus == "👄" {
-            language = "en-us"
-        }
-        utterance.voice = AVSpeechSynthesisVoice(language: language)
-        utterance.rate = 0.5
+    func black() {
+        //self.view.backgroundColor = colors.black
         
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
+        let colorTop =  colors.blackStart.cgColor
+        let colorBottom = colors.black.cgColor
         
-    }
-    
-    @IBAction func stop(_ sender: Any) {
-        paused = true;
-        startButton.setTitle("Start",for: .normal)
-        timer.invalidate()
-        timer2.invalidate()
-        timerLabel.text = "00:00.00"
-        progressView.setProgress(0, animated: false)
-    }
-    
-    func parseDuration(_ timeString:String) -> TimeInterval {
-        guard !timeString.isEmpty else {
-            return 0
-        }
+        gradient.colors = [ colorTop, colorBottom]
+        gradient.locations = [ 0.0, 1.0]
+        gradient.frame = self.view.bounds
         
-        var interval:Double = 0
-        
-        let parts = timeString.components(separatedBy: ":")
-        for (index, part) in parts.reversed().enumerated() {
-            interval += (Double(part) ?? 0) * pow(Double(60), Double(index))
-        }
-        
-        return interval
-    }
-    
-    func updateCounter() {
-        var elapsedTime: TimeInterval = NSDate.timeIntervalSinceReferenceDate - startTime
-        
-        var elapsedTime2: TimeInterval = NSDate.timeIntervalSinceReferenceDate - startTime2
-        //print(elapsedTime2)
-        var length = activity.count - 1
-        //print(length)
-        
-        var tempRound = round(elapsedTime)
-        //print(tempRound)
-        if (tempRound.truncatingRemainder(dividingBy: Double(interval)) == 0 && round(elapsedTime) != 0) {
-            if voiceStatus == "🔊" {
-                playSound()
-            } else if voiceStatus == "👄" || voiceStatus == "🇬🇧"{
-                speak(word: activity[instance])
-                
-                //speak(word: "\(instance)")
-                //print("THIS IS IIIIIIIII: \(instance)")
-            }
-            updateActivityText()
-            progressView.progress = 0.0
-            flashScreen()
-            instance += 1
-            if instance == length {
-                instance = 0
-            }
-        }
-        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        transition(item: self.view)
         
     }
     
-    
-    
-    func updateActivityText() {
-        self.currentActivityLabel.text = activity[instance]
-        var c = instance + 1;
-        if c == activity.count {
-            c=0
-        }
-        self.nextActivityLabel.text = activity[c]
+    func white() {
+        gradient.removeFromSuperlayer()
+        self.view.backgroundColor = colors.white
+        transition(item: self.view)
     }
     
-    func updateCounterText() {
-        
-        var elapsedTime: TimeInterval = NSDate.timeIntervalSinceReferenceDate - startTime
-        
-        var elapsedTime2: TimeInterval = NSDate.timeIntervalSinceReferenceDate - startTime2
-        if paused == true {
-            elapsedTime = elapsedTime + pausedTime
-        }
-        let minutes = UInt8(elapsedTime / 60.0)
-        elapsedTime -= (TimeInterval(minutes) * 60)
-        let seconds = UInt8(elapsedTime)
-        elapsedTime -= TimeInterval(seconds)
-        let milli = UInt8(elapsedTime * 100)
-        
-        
-        //main label:
-        timerLabel.text = "\(String(format: "%02d", minutes)):\(String(format: "%02d", seconds)).\(String(format: "%02d", milli))"
-        
-        //progress view:
-        progressView.progress = Float(seconds).truncatingRemainder(dividingBy: Float(interval)) / Float(interval)
+    func transition(item: UIView) {
+        UIView.transition(with: item,
+                          duration: 0.25,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "horn", withExtension: "mp3")!
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            guard let player = player else { return }
-            
-            player.prepareToPlay()
-            player.play()
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    
-    func intervalSelect(_ gesture: UIGestureRecognizer) {
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizerDirection.right: //-1
-                if (i - 1 > -1) {
-                    i -= 1
-                }
-            case UISwipeGestureRecognizerDirection.left: //+1
-                if (i + 1 != intervalOptions.count) {
-                    i += 1
-                }
-            default:
-                break
-            }
-            print("after: \(i)")
-            interval = intervalOptions[i]
-            intervalText.text = "\(interval)"
-        }
-    }
-    
-    func musicControl(_ gesture: UIGestureRecognizer) {
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizerDirection.up:
-                if (z - 1 > -1) {
-                    z -= 1
-                }
-            case UISwipeGestureRecognizerDirection.down:
-                if (z + 1 != voiceOptions.count) {
-                    z += 1
-                }
-            default:
-                break
-            }
-            voiceStatus = voiceOptions[z]
-            musicStatus.text = voiceStatus
-        }
-    }
-    
-    func flashScreen() {
-        let screenSize = UIScreen.main.bounds
-        let frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
-        let shutterView = UIView(frame: frame)
-        shutterView.backgroundColor = UIColor.white
-        view.addSubview(shutterView)
-        UIView.animate(withDuration: 0.3, animations: {
-            shutterView.alpha = 0.25
-        }, completion: { (_) in
-            shutterView.removeFromSuperview()
-        })
-        print("flashed")
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == workoutSegueIdentifier {
-            let destination = segue.destination as? workoutTableViewController
-            destination?.activity = activity
-            destination?.time = time
-            destination?.text = "yo"
-        }
-    }
-    
-    func get_data_from_url(_ link:String) {
-        let url:URL = URL(string: link)!
-        let session = URLSession.shared
-        
-        let request = NSMutableURLRequest(url: url)
-        request.httpMethod = "GET"
-        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
-        
-        
-        let task = session.dataTask(with: request as URLRequest, completionHandler: {
-            (
-            data, response, error) in
-            
-            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
-                
-                return
-            }
-            self.extract_json(data!)
-        })
-        task.resume()
-    }
-    
-    func extract_json(_ data: Data) {
-        let json: Any?
-        
-        do {
-            json = try JSONSerialization.jsonObject(with: data, options: [])
-        } catch {
-            return
-        }
-        
-        guard let data_list = json as? NSArray else {
-            return
-        }
-        
-        
-        if let workout_data = json as? NSArray {
-            for i in 0 ..< data_list.count {
-                if let entry = workout_data[i] as? NSDictionary {
-                    if let timeEntry = entry["time"] as? String {
-                        if let activityEntry = entry["activity"] as? String  {
-                            time.append(timeEntry)
-                            activity.append(activityEntry)
-                        }
-                    }
-                }
-            }
-        }
-        
-        DispatchQueue.main.async(execute: {self.refresh()})
-    }
-    
-    func refresh()  {
-        //self.tableView.reloadData()
-    }
+    override var prefersStatusBarHidden: Bool {  
+        return true  
+    }  
     
 }
---
+++ interval-timer/master.swift
@@ -0,0 +1,23 @@
+//
+//  master.swift
+//  interval-timer
+//
+//  Created by Babbs, Dylan on 6/29/17.
+//  Copyright © 2017 Babbs, Dylan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var track = false
+
+struct colors {
+    static let white = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+    static let red = UIColor(red:1.00, green:0.14, blue:0.22, alpha:1.0)
+    static let blue = UIColor(red:0.24, green:0.31, blue:0.88, alpha:1.0)
+    static let grey = UIColor(red:0.50, green:0.55, blue:0.64, alpha:1.0)
+    static let lightGrey = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0)
+    
+    static let blackStart = UIColor(red:0.22, green:0.22, blue:0.22, alpha:1.0)
+    static let black = UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0)
+}
